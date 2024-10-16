ca92222623eeb9945a5d6fb95de021ce9d10f53d
safiul0073 <mdsafiul0073@gmail.com>
2024-10-16 23:28:18 +0600

    build error fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "png.pngtree.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "starting.sinvestingsolutions.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/api/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/(admin)/transaction/page.tsx
+++ src/app/(admin)/transaction/page.tsx
@@ -5,12 +5,7 @@ import { useTransferHistory } from "@/hooks/query/publicUser/getTransferHistory"
 const Transaction = () => {
   const { data: history, isLoading, refetch } = useTransferHistory();
 
-  return (
-    <div>
-      {console.log(history?.data)}
-      Transaction
-    </div>
-  );
+  return <div>Transaction</div>;
 };
 
 export default Transaction;
